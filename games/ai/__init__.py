58b2ccce604e17bce60377cbed036a1cbe09f61c
EvilRedOctober <MoJo97@yandex.ru>
2021-09-22 19:05:49 +0300

    Add init

---
+++ games/ai/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""Contains functions for Implementations of AI"""
+
+pass
---
+++ gui/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Contains GUI forms and logic
+    'forms/' - Classes of forms with GUI elements
+    'logic/' - Classes inherited from forms, supplemented with logic"""
+
+pass
